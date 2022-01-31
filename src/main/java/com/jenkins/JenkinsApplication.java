4ad423a224469dee0b758b87990a78385c6d7c77
yglee <yglee@ksmartpia.com>
2022-01-31 09:35:31 +0900

    jenkins test

--- src/main/java/com/jenkins/JenkinsApplication.java
+++ src/main/java/com/jenkins/JenkinsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsApplication {
 
+    //Jenkins Test
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
     }
